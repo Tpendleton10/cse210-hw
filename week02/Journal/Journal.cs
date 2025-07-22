c5f67362d90422892a8a3b1a9fa3ec8fc97a7d93
Ty Pendleton <Pen24031@byui.edu>
2025-07-22 15:19:18 -0700

    changes

---
+++ week02/Journal/Journal.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+
+public class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        // Empty stub
+    }
+
+    public void DisplayAll()
+    {
+        // Empty stub
+    }
+
+    public void SaveToFile(string filename)
+    {
+        // Empty stub
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        // Empty stub
+    }
+}
---
+++ week02/Journal/PromptGenerator.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+public class PromptGenerator
+{
+    private List<string> _prompts = new List<string>();
+
+    public string GetRandomPrompt()
+    {
+        return ""; // Return empty string for stub
+    }
+}
